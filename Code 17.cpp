00e6075d46ab7fffe3bd3e1ef13718a62a7fc64c
raogourav2599-ops <raogourav2599@gmail.com>
2025-11-26 22:31:47 +0530

    Add files via upload

---
+++ Code 17.cpp
@@ -0,0 +1,34 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter value for x square %d",a);
+	scanf("%d",&a);
+	
+	printf("Enter value for x %d",b);
+	scanf("%d",&b);
+	
+	printf("Enter value for c %d",c);
+	scanf("%d",&c);
+	
+	int d;
+	d = (b*b-4*a*c);
+	
+	int root1,root2;
+	root1 = (-b+sqrt(d))/2*a;
+	root2 = (-b-sqrt(d))/2*a;
+	
+	if (d>0)
+	printf("Roots are real and different %d,%d",root1,root2);
+	
+	else if (d<0)
+	printf("Roots are imaginery and complex");
+	
+	else if (d==0)
+	printf("Roots are Real and Same %d",root1);
+	
+	return 0;
+}
+
